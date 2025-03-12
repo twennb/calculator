8891bd1b7f4af1723ab503d75adaed31cd33cf0f
twennb <tom.wennberg@hotmail.com>
2025-03-12 11:08:59 +0100

    added the needed functions for desired mathematical operations

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,5 +1,43 @@
 """a simple calculator application that runs in the command line"""
 
+# Calculator features:
+# addition
+# subtraction
+# multiplication
+# division
+# square
+# root
+# modulo
+#
+
+
+def add(x, y):
+    """function returns x + y"""
+
+
+def subtract(x, y):
+    """function returns x - y"""
+
+
+def multiply(x, y):
+    """function returns x multiplied by y"""
+
+
+def divide(x, y):
+    """function returns x divided by y"""
+
+
+def square(x):
+    """function returns squared value x"""
+
+
+def root(x):
+    """function returns the root of value x"""
+
+
+def modulo(x, y):
+    """function returns x % y"""
+
 
 def main():
     """the main application function"""
